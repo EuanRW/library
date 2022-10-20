412ff82f2baa2ddaabd430d02768fcfdd44178fc
Euan Williams <euanwilliams00@gmail.com>
2022-10-21 00:15:39 +0100

    Adding ability to view book details in UI.

--- script.js
+++ script.js
@@ -22,13 +22,56 @@ Book.prototype.info = function () {
 };
 
 function addBookToLibrary(book) {
-  myLibrary.push(book)
+  myLibrary.push(book);
+
+  const bookGrid = document.getElementById("bookGrid");
+
+  const bookGridItem = document.createElement("div");
+  bookGridItem.className = "bookGridItem"
+
+  bookGrid.appendChild(bookGridItem);
+
+  //title
+  const titlePara = document.createElement("p");
+  const titleNode = document.createTextNode(book.title);
+  titlePara.appendChild(titleNode);
+  titlePara.className = "bookTitle"
+
+  //author
+  const authorPara = document.createElement("p");
+  const authorNode = document.createTextNode(book.author);
+  authorPara.appendChild(authorNode);
+  authorPara.className = "bookAuthor"
+
+  //page count
+  const pagesPara = document.createElement("p");
+  const pagesNode = document.createTextNode("Pages: " + book.pages);
+  pagesPara.appendChild(pagesNode);
+  pagesPara.className = "bookPages"
+
+  //read status
+  const readPara = document.createElement("p");
+  const readNode = document.createTextNode(book.read);
+  readPara.appendChild(readNode);
+  readPara.className = "bookReadStatus"
+
+  bookGridItem.appendChild(titlePara);
+  bookGridItem.appendChild(authorPara);
+  bookGridItem.appendChild(pagesPara);
+  bookGridItem.appendChild(readPara);
+
+
 }
 
 const book1 = new Book("The Hobit", "J.R.R Tolkien", 310, false);
 const book2 = new Book("A Promised Land", "Barrack Obama", 700, false);
-const book3 = new Book("All Quiet On The Western Front", "Erich Maria Remarque", 200, false)
+const book3 = new Book(
+  "All Quiet On The Western Front",
+  "Erich Maria Remarque",
+  200,
+  false
+);
 
-addBookToLibrary(book1)
-addBookToLibrary(book2)
-addBookToLibrary(book3)
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
